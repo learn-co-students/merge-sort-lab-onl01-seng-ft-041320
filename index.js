1b9242eed3823cd8bec5e96424e9ebae224103b2
ReginaF2012 <skritschy@gmail.com>
2020-12-14 15:50:36 -0600

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-function findMinAndRemoveSorted(array){
-    return array.shift()
+function findMinAndRemoveSorted(arr){
+    return arr.shift();
 }
 
-
-function merge(firstHalf, secondHalf){
-    let sorted = []
-    while(firstHalf.length !== 0 && secondHalf.length !== 0){
-        if(firstHalf[0] < secondHalf[0]){
-            sorted.push(firstHalf.shift())
+function merge(arr1, arr2){
+    let sorted = [];
+    while(arr1.length > 0 && arr2.length > 0){
+        if(arr1[0] < arr2[0]){
+            sorted.push(findMinAndRemoveSorted(arr1));
         } else {
-            sorted.push(secondHalf.shift())
+            sorted.push(findMinAndRemoveSorted(arr2));
         }
     }
-    return sorted.concat(firstHalf).concat(secondHalf)
+    return sorted.concat(arr1).concat(arr2)
 }
 
 function mergeSort(array){
     let midpoint = array.length/2
     let firstHalf = array.slice(0, midpoint)
     let secondHalf = array.slice(midpoint, array.length)
-    let sorted;
-  
+ 
     if(array.length < 2){
       return array
     } else {
-      sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf))
+      return merge(mergeSort(firstHalf), mergeSort(secondHalf))
     }
-    return sorted
-  }
+}
